1f0dc1751e04582c17504f24a46a815a5ae515bb
Allan Napier <49874689+allannapier@users.noreply.github.com>
2021-01-10 12:02:10 +0000

    Update PasswordGen.py

--- PasswordGen.py
+++ PasswordGen.py
@@ -3,37 +3,62 @@ import random
 import sys
 import pyperclip
 
+class Password:
+
+    length = 0
+    strength = 0
+    value = ''
+
+    def __init__(self, length,strength):
+        self.length = length
+        self.strength = strength
+        
+        
+    
+
+    
+    def generate_password(self):
+        x=0
+        value = ''
+        while x < int(self.length) :
+            x = x + 1
+            #value = str(value) + str(randint(0, 10))
+            ucLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            myNumbers = '123456789'
+            lcLetters = 'abcdefghijklmnopqrstuvwxyz'
+            pCharacters = '£$%&-+={}[]'
+            value = value + random.choices(ucLetters, k=1)[0] 
+            if(x<int(self.length)):
+                if(self.strength > 1) :
+                    value = value + random.choices(lcLetters, k=1)[0] 
+                    x = x + 1
+            if(x<int(self.length)):
+                if(self.strength > 1) :
+                    value = value + random.choices(myNumbers, k=1)[0] 
+                    x = x + 1
+            if(x<int(self.length)):
+                if(self.strength == 3):
+                    value = value + random.choices(pCharacters, k=1)[0] 
+                    x = x + 1
+            self.value = value
+        return(value)
+
+    def password_to_clibpoard(self):
+        print(self.value)
+        pyperclip.copy(self.value)
+        pyperclip.paste()
+
 if(len(sys.argv)>1):
-    y = int(sys.argv[1])
-    strength = int(sys.argv[2])
+    l = int(sys.argv[1])
+    s = int(sys.argv[2])
 else:
-    y = int(input("How many characters should the password have?"))
-    strength = int(input("How strong should the password be? 1 = medium 2 = Strong = 3 = Ultra"))
+    l = input("How many characters should the password have?")
+    s = int(input("How strong should the password be? 1 = medium 2 = Strong = 3 = Ultra"))    
 
-value = ""
-x = 0
+newpass = Password(l,s)
+print(newpass.generate_password())
+newpass.password_to_clibpoard()
 
-while x < y :
-    x = x + 1
-    #value = str(value) + str(randint(0, 10))
-    ucLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    myNumbers = '123456789'
-    lcLetters = 'abcdefghijklmnopqrstuvwxyz'
-    pCharacters = '£$%&-+={}[]'
-    value = value + random.choices(ucLetters, k=1)[0] 
-    if(x<y):
-        if(strength > 1) :
-            value = value + random.choices(lcLetters, k=1)[0] 
-            x = x + 1
-    if(x<y):
-        if(strength > 1) :
-            value = value + random.choices(myNumbers, k=1)[0] 
-            x = x + 1
-    if(x<y):
-        if(strength == 3):
-            value = value + random.choices(pCharacters, k=1)[0] 
-            x = x + 1
 
-pyperclip.copy(value)
-pyperclip.paste()
-print(value)  
+
+#print(value)  
